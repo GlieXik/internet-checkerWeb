94b0a07367b6c33cdbafbb2e913f5f60ca524cfa
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-12-08 00:36:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ import Signal from "../components/signal";
 import axios from "axios";
 export const getStaticProps = async () => {
   const res = await axios(process.env.URL);
-
+  if (!res) {
+    return {
+      noFound: null, // will be passed to the page component as props
+    };
+  }
   return {
     props: { status: res.data.status }, // will be passed to the page component as props
   };
